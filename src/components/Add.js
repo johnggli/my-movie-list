29a9ac7dd1d6b697d58a3fa66f23829679892771
John Emerson <john.carv.sousa@gmail.com>
2021-12-02 17:32:41 -0300

    feat: add useState in Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Add = () => {
+  const [query, setQuery] = useState("");
+
+  const onChange = e => {
+    e.preventDefault();
+    
+    setQuery(e.target.value);
+  }
+
   return (
     <div className="add-page">
       <div className="container">
         <div className="add-content">
           <div className="input-wrapper">
-            <input type="text" placeholder="Search for a movie" />
+            <input
+              type="text"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={onChange} />
           </div>
         </div>
       </div>
