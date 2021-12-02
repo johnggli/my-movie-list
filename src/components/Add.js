b3c10bb0988ada33df41d68e3ed8dccfcf08549a
John Emerson <john.carv.sousa@gmail.com>
2021-12-02 17:27:12 -0300

    feat: add input for add screen

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export const Add = () => {
   return (
-    <div>
-      <h1>Add Page</h1>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input type="text" placeholder="Search for a movie" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
